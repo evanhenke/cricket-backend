39b5a521f5533ac8102a14dee61c5794b394ba1b
Evan Henke <evanjhenke@gmail.com>
2018-07-12 19:28:00 -0500

    adding cors on router..

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const User = require('./User.js');
 const Book = require('./Book.js');
 const Authentication = require('./Authentication/Authentication');
@@ -6,6 +7,7 @@ const Authentication = require('./Authentication/Authentication');
 const app = express.Router();
 
 module.exports = function () {
+  app.use(cors());
   app.use(User());
   app.use(Book());
   app.use(Authentication());
