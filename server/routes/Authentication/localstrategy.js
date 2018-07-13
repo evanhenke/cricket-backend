24fa53dc0d3e2c78c089784cd0ad1193331439ed
Evan Henke <evanjhenke@gmail.com>
2018-07-12 19:16:21 -0500

    stopped using error handler on local strategy

--- server/routes/Authentication/localstrategy.js
+++ server/routes/Authentication/localstrategy.js
@@ -1,18 +1,16 @@
 const passport = require('passport');
 const { Strategy } = require('passport-local');
 const User = require('./../../schemas/UserSchema');
-const ErrorHandler = require('./../../error/ErrorHandler')();
 const Authenticator = require('./Authenticator')();
 
 function localStrategy() {
   passport.use(new Strategy((username, password, done) => {
     User.findByUsername(username, (error, user) => {
       if (error) {
-        ErrorHandler.handle(error);
+        done(error);
       } else {
         Authenticator.authenticate(user, password, (err, authentic) => {
           if (err) {
-            ErrorHandler.handle(err);
             return done(err);
           }
           if (!authentic) {
